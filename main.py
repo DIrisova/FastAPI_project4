7be984ad34fbe7c9e027047b05cea46d06eda465
DIrisova <irisovadilobar@gmail.com>
2023-03-25 18:41:47 -0400

    add updates

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def index():
     #return {'data': {'name': 'Dilobar'}}
     return {'data': 'blog list'}
 
+@app.get("/blog/unpublished")
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
 @app.get("/blog/{id}")
-def show(id):
+def show(id: int): #int is converting id to the integer. In case we want string we can simply use str instead of int
     return {'data': id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    #fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
