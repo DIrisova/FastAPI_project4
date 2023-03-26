89beb6bdce41aaeb5b0fad5a397841e1864f82d3
DIrisova <irisovadilobar@gmail.com>
2023-03-25 22:55:27 -0400

    add updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -21,7 +22,31 @@ def show(id: int): #int is converting id to the integer. In case we want string 
     return {'data': id}
 
 
-@app.get("/blog/{id}/comments")  #there is anything given in the path and that is accepted here so this this could be the path parameter otherwise you are talking about obviously the query parameter 
+@app.get("/blog/{id}/comments")  #there is anything given in the path and that is accepted here so this this could be the path parameter otherwise you are talking about the query parameter 
 def comments(id, limit=10):
     #fetch comments of blog with id = id
+    #return limit
     return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'success 200 yahoo title as {request.title}'}
+
+
+
+
+
+
+
+
+
+
+
+
