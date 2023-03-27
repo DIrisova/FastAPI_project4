4f2fba615268c7339921653d4e0f2370bc77d2a8
DIrisova <irisovadilobar@gmail.com>
2023-03-27 10:27:27 -0400

    Store blog to database

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from . import schemas, models
 from .database import engine 
+from sqlalchemy.orm import Session 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
-def create(request: schemas.Blog):
+def create(request: schemas.Blog, db: Session = Depends(get_db)): #getting error
     return request
 
 
