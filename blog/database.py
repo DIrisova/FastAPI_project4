bb16236719e74835a4af8031933420604b4a09a0
DIrisova <irisovadilobar@gmail.com>
2023-03-27 15:41:58 -0400

    Store blog

--- blog/database.py
+++ blog/database.py
@@ -4,13 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
-
 SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db' #defining database URL 
 
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread": False}) # creating engine
 
-Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+#Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
