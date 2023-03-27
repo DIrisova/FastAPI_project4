0d828756ed6163bff3bf1268290e4f272cf6b520
DIrisova <irisovadilobar@gmail.com>
2023-03-27 18:30:14 -0400

    Exception & Status Code

--- blog/database.py
+++ blog/database.py
@@ -9,7 +9,7 @@ SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db' #defining database URL
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread": False}) # creating engine
 
-#Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
